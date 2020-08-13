72f21245ef55eab29f255b454c0863252fd43f9f
subedisapana <subedisapana10@gmail.com>
2020-08-13 17:12:22 +0545

    signin form with validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import {Container, Row, Col} from 'reactstrap';
+import Header from './components/shared/Header/header.component';
+import Registration from './components/registration/registration.component';
+import Footer from './components/shared/Footer/footer.component';
+
 
 function App() {
     return (
-        <div>
-            Hello World!!!
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                <Header/>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col md={3}>
+                <Registration/>
+                </Col>
+            </Row>
+
+
+            <Row>
+                <Col>
+                <Footer/>
+                </Col>
+            </Row>
+
+        </Container>
     );
 }
 
