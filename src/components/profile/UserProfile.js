b9244dff413c8ff8e9b9c6cd52b947ecbb008c83
Neha Pokharel <nehapokharel008@gmail.com>
2020-08-24 16:21:51 +0545

    fixes: changed class to className

--- src/components/profile/UserProfile.js
+++ src/components/profile/UserProfile.js
@@ -3,20 +3,20 @@ import React from 'react';
 class UserProfile extends React.Component{
     render(){
         return(   
-            <div class="container mt-5 ml-0">
-                <div class="row">
-                <div class="col">
+            <div className="container mt-5 ml-0">
+                <div className="row">
+                <div className="col">
                     <div className="card" style={{width: "18rem"}}>
                         <img className="card-img-top" src="https://www.pngitem.com/pimgs/m/35-350426_profile-icon-png-default-profile-picture-png-transparent.png" alt="Profile Image"/>
                         <div className="card-body">
-                            <p class="card-text">bio here!!</p>
+                            <p className="card-text">bio here!!</p>
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div className="col">
                     <div className="card">
-                        <h5 class="card-header font-weight-bold">User Information</h5>
-                        <div class="card-body">
+                        <h5 className="card-header font-weight-bold">User Information</h5>
+                        <div className="card-body">
                             Information heree!!                         
                         </div>
                     </div>
