7e0824029f4c309fee2fcfc2f8d96da23a096906
Neha Pokharel <nehapokharel008@gmail.com>
2020-08-27 19:35:55 +0545

    fixes: auth header

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ const api = axios.create({
 
 api.interceptors.request.use(config => {
     if (localStorage.getItem('token')) {
-        config.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
+        config.headers.Authorization = 'token ' + localStorage.getItem('token');
     }
     return config;
 })
