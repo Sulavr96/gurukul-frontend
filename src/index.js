846cdd5516a1a855bfadb71ab29d63692988ff72
subedisapana <subedisapana10@gmail.com>
2020-09-05 09:18:46 +0545

    added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './store';
-import { BrowserRouter as Router, Switch , Route, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './components/registration/login_component';
 import Registration from './components/registration/registration.component';
 import ForgotPassword from './components/registration/forgot_password.component';
@@ -16,15 +16,14 @@ import ResetPassword from './components/registration/reset_password.component';
 import Notice from './components/notice/notice_component';
 import T_profile from './components/dashboard/teacher/t_profile';
 import Courses from './components/dashboard/teacher/courses/course';
-import Home from './components/homepage/home'
+import Home from './components/homepage/home';
 
-
-const AuthenticatedRoute = ({component:Component, ...rest})=>{
+const AuthenticatedRoute = ({ component: Component, ...rest }) => {
     const token = localStorage.getItem('token');
     return <Route
         {...rest}
-        render ={(props) => (
-            !!token ? <Component {...props}/>
+        render={(props) => (
+            !!token ? <Component {...props} />
                 : <Redirect
                     to='/login'
                 />
@@ -39,21 +38,19 @@ ReactDOM.render(
     <Provider store={store}>
         <Router>
             <Switch>
-                <Route exact path="/" component={Home}/>
-=
-                <Route exact path="/login/" component={Login}/>
-                <Route exact path="/signup/" component={Registration}/>  
-                <Route exact path="/forgotpassword/" component={ForgotPassword}/>
-                <Route exact path="/password-reset/" component={ResetPassword}/>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login/" component={Login} />
+                <Route exact path="/signup/" component={Registration} />
+                <Route exact path="/forgotpassword/" component={ForgotPassword} />
+                <Route exact path="/password-reset/" component={ResetPassword} />
+                
+                <AuthenticatedRoute path="/" component={App} />
 
-                <Route path="/user/notice/" component={Notice} />
-                <Route path="/teacher/profile" component={T_profile} />
-                <Route path="/teacher/courses/" component={Courses} />
-                <AuthenticatedRoute path="/" component={App}/>
-             
 
             </Switch>
         </Router>
+
+        
     </Provider>,
     document.getElementById('root')
 );
