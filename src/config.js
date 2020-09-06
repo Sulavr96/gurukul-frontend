a9d2219d05071d51a90edaee3f99e56c2925f048
sulav <cyberraid@gmail.com>
2020-09-06 18:00:42 +0545

    [api-endpoint]

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 let config =
     {
         //Insert backend url here!!
-        backendURL : 'http://127.0.0.1:8000/'
+        // backendURL : 'http://127.0.0.1:8000/'
+        backendURL : 'https://nscubegurukul.herokuapp.com/'
     }
 
 export default config;
